f09025d98a8f1e9415c161d642eff0ec245c3871
PRATHAMESHGAWAS <prathameshgawas87@gmail.com>
2020-12-11 21:12:42 +0530

    restrict access to /services/*

--- routes/Services.js
+++ routes/Services.js
@@ -5,8 +5,13 @@ const edroute = require('./Services/Edroute')
 const textbook = require('./Services/tbRenting')
 const forum = require('./Services/forum')
 
-router.use('/',edroute)
-router.use('/',textbook)
-router.use('/forum',forum)
+var sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) next();
+  else res.redirect("/login");
+};
+
+router.use('/',sessionChecker,edroute)
+router.use('/',sessionChecker,textbook)
+router.use('/forum',sessionChecker,forum)
 
 module.exports = router
