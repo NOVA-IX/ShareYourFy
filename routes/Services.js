4e278d6376ab7a782bcd8abc2f2277e3d07b1602
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-05-04 23:45:01 +0530

    Add Route for PYQ Module

--- routes/Services.js
+++ routes/Services.js
@@ -4,6 +4,7 @@ const edroute = require('./Services/Edroute')
 const textbook = require('./Services/tbRenting')
 const forum = require('./Services/forum')
 const extra = require('./Services/extra')
+const pyq = require('./Services/pyq')
 const ebooks = require('./Services/ebooks')
 const { checkUser } = require('../middlewares/authorization')
 
@@ -11,6 +12,7 @@ router.use('/ed', checkUser, edroute)
 router.use('/textbook', checkUser, textbook)
 router.use('/forum', checkUser, forum)
 router.use('/extra', checkUser, extra)
+router.use('/pyq', checkUser, pyq)
 router.use('/ebooks', checkUser, ebooks)
 
 module.exports = router
